513782f6e07c37ed62d308dcf74d55a5ecc1d9e0
lauren_kong_mako_22 <103288573+lauren-kong@users.noreply.github.com>
2022-07-05 09:35:22 +1200

    redux setup

--- client/index.js
+++ client/index.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import {
+  legacy_createStore as createStore,
+  applyMiddleware,
+  compose,
+} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 
 import App from './components/App'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
+
 document.addEventListener('DOMContentLoaded', () => {
   render(
-    <Router>
-      <App />
-    </Router>,
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
     document.getElementById('app')
   )
 })
